59248d65973733edb83ee500e940b1b888ba5a2c
Matheus Campos <matheusfreitascampos@gmail.com>
2021-02-24 22:39:00 -0300

    modificacao no profile(perfil)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,16 @@
-import styles from '../styles/components/Profile.module.css';
+import styles from '../assets/styles/components/profile.module.css'
 
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/MatheusFC2.png/" alt="Matheus Campos"/>
+      <img src="https://github.com/MatheusFC2.png" alt="Matheus Campos" />
       <div>
         <strong>Matheus Campos</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          <img src="icons/level.svg" alt="Level" />
+          level 1
         </p>
       </div>
     </div>
-  );
+  )
 }
