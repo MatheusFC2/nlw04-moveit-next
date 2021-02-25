95ad294d62876f65f7b6fba4e4730d413d787534
Matheus Campos <matheusfreitascampos@gmail.com>
2021-02-24 22:35:45 -0300

    troquei a aplicacao para nextjs

---
+++ src/components/completed-challenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../assets/styles/components/completed-challenges.module.css'
+
+export function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios Completos</span>
+      <span>67</span>
+    </div>
+  )
+}
